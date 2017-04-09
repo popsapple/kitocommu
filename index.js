823cb7c164f776ebb54e5f22e9ec0562cb4ae2e0
popsapple <saysun34@gmail.com>
2017-04-09 20:43:47 +0900

    ㅇ

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ request({
     url: url + queryParams,
     method: 'GET'
 }, function (error, response, body) {
-    //console.log('Status', response.statusCode);
+    console.log('Status', response.statusCode);
     //console.log('Headers', JSON.stringify(response.headers));
     //console.log('Reponse received', body);
 });
