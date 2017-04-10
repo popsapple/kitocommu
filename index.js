93aa03178fc066f08ca1abdcdf5c6ceb43d1f986
popsapple <saysun34@gmail.com>
2017-04-11 00:23:38 +0900

    식품

--- index.js
+++ index.js
@@ -18,21 +18,22 @@ app.listen(app.get('port'), function() {
 });
 
 
-var request = require('request');
-app.get('/', function(request, response) {
-  var url = 'http://apis.data.go.kr/1470000/FoodNtrIrdntInfoService/getFoodNtrItdntList';
-  var queryParams = '?' + encodeURIComponent('ServiceKey') + '=73Jk1pUmlpaKcb3esV43IVrmH4SVea%2FGn%2BXrgSkI4Mk16R0KYIrrPKwuGPS2qZAJPIk4JcxUIy7N3l0Sh6eBIQ%3D%3D'; /* Service Key*/
-  queryParams += '&' + encodeURIComponent('desc_kor') + '=' + encodeURIComponent('김치'); /* 식품이름 */
-  queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* 페이지번호 */
-  queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('1'); /* 한 페이지 결과 수 */
+/* NodeJs 샘플 코드 */
 
-  request({
-      url: url + queryParams,
-      method: 'GET'
-  }, function (error, response, body) {
-      console.log('Status', response.statusCode);
-      console.log('Headers', JSON.stringify(response.headers));
-      console.log('Reponse received', response.body);
-      //response.render('pages/index',body);
-  });
+
+var request = require('request');
+
+var url = 'http://apis.data.go.kr/1470000/FoodNtrIrdntInfoService/getFoodNtrItdntList';
+var queryParams = '?' + encodeURIComponent('ServiceKey') + '=73Jk1pUmlpaKcb3esV43IVrmH4SVea%2FGn%2BXrgSkI4Mk16R0KYIrrPKwuGPS2qZAJPIk4JcxUIy7N3l0Sh6eBIQ%3D%3D'; /* Service Key*/
+queryParams += '&' + encodeURIComponent('desc_kor') + '=' + encodeURIComponent('치즈라면'); /* 식품이름 */
+queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* 페이지번호 */
+queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('3'); /* 한 페이지 결과 수 */
+
+request({
+    url: url + queryParams,
+    method: 'GET'
+}, function (error, response, body) {
+    console.log('Status', response.statusCode);
+    console.log('Headers', JSON.stringify(response.headers));
+    console.log('Reponse received', body);
 });
