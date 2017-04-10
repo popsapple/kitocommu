ffa43c55cecf12865a4d7f9613985bf3e3ca3ac8
popsapple <saysun34@gmail.com>
2017-04-11 00:32:43 +0900

    ㅇ

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ var request = require('request');
 
 var url = 'http://apis.data.go.kr/1470000/FoodNtrIrdntInfoService/getFoodNtrItdntList';
 var queryParams = '?' + encodeURIComponent('ServiceKey') + '=73Jk1pUmlpaKcb3esV43IVrmH4SVea%2FGn%2BXrgSkI4Mk16R0KYIrrPKwuGPS2qZAJPIk4JcxUIy7N3l0Sh6eBIQ%3D%3D'; /* Service Key*/
-queryParams += '&' + encodeURIComponent('desc_kor') + '=' + encodeURIComponent('김치찌개'); /* 식품이름 */
-queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* 페이지번호 */
-queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('3'); /* 한 페이지 결과 수 */
+queryParams += '&' + encodeURIComponent('desc_kor') + '=' + encodeURIComponent('짜장면'); /* 식품이름 */
+queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent(''); /* 페이지번호 */
+queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent(''); /* 한 페이지 결과 수 */
 
 request({
     url: url + queryParams,
