c2a0d0cade7343c960058ea7cd4c66f410898d98
popsapple <saysun34@gmail.com>
2017-04-11 01:31:07 +0900

    ㅓ

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ request({
     url: url + queryParams,
     method: 'GET',
     headers : {
-          "x-waple-authorization" : 'MzY4LTE0OTE4NDE3MDg3NzUtMjVkNzNiMmYtZjQ3Ni00OTRiLTk3M2ItMmZmNDc2Mjk0YmI5'
-      }
+          "x-waple-authorization" : 'MzY4LTE0OTE4NDE3MDg3NzUtMjVkNzNiMmYtZjQ3Ni00OTRiLTk3M2ItMmZmNDc2Mjk0YmI5',
+          "content-type":"application/x-www-form-urlencoded; charset=UTF-8"}
 }, function (error, response, body) {
     console.log('Status', response.statusCode);
     console.log('Headers', JSON.stringify(response.headers));
