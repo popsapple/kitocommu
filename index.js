6831afb7c98f676dc0c33f9c5a06f3192cd8d15f
popsapple <saysun34@gmail.com>
2017-04-13 01:09:59 +0900

    d

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var app = express();
-var xml2js = require('xml2js');
-var parser = new xml2js.Parser();
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -11,20 +9,12 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.use(function(req, res, next) {
-  res.locals.request = req;
-  next();
-});
+var request = require('request');
 
 var url = 'http://api.dbstore.or.kr:8880/foodinfo/search.do';
 var queryParams = '?' + encodeURIComponent('uid') + '=' + encodeURIComponent('LQUV6MOX');
 queryParams += '&' + encodeURIComponent('w') + '=' + encodeURIComponent('유기농');
 
-var request = require('request');
 request({
     url: url + queryParams,
     method: 'GET',
@@ -32,17 +22,16 @@ request({
           "x-waple-authorization" : "MzY4LTE0OTE4NDE3MDg3NzUtMjVkNzNiMmYtZjQ3Ni00OTRiLTk3M2ItMmZmNDc2Mjk0YmI5",
           "content-type":"application/x-www-form-urlencoded; charset=UTF-8"}
 }, function (error, response, body) {
-  //var data = JSON.parse(body);
-    console.log(body);
-  //response.render('pages/index', { body: body });
+    console.log('Reponse received', body);
+    var data = body;
+    data = JSON.parse(data);
+    response.render('pages/index', { data: data });
 });
-
-});
-
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
 
 /* NodeJs 식품정보 아이디 갖고오기....
 
