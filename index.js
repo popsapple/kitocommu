4cdc5babbf14af30a0eeb99de229a6a5061f6e9d
popsapple <saysun34@gmail.com>
2017-04-13 01:00:52 +0900

    d

--- index.js
+++ index.js
@@ -32,9 +32,8 @@ request({
           "x-waple-authorization" : "MzY4LTE0OTE4NDE3MDg3NzUtMjVkNzNiMmYtZjQ3Ni00OTRiLTk3M2ItMmZmNDc2Mjk0YmI5",
           "content-type":"application/x-www-form-urlencoded; charset=UTF-8"}
 }, function (error, response, body) {
-  body = JSON.parse(body);
-    console.log(body);
-  //response.render('pages/index', { body: body });
+
+  response.render('pages/index', { body: body });
 });
 
 });
