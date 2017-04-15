ab539df2eb2d0338b755c5c2dd603aeb778c96b7
popsapple <saysun34@gmail.com>
2017-04-15 17:21:30 +0900

    ㅇ
    ㅇ

--- index.js
+++ index.js
@@ -19,15 +19,16 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response)  {
   var user_keyword;
   request.keyword ? user_keyword = request.keyword : user_keyword = '유기농';
+  JSON.stringify(request.body);
   if(request.keyword){
-  console.log('11111111111111111111111111111111111 ::'+request.keyword);
-}
-if(request.body){
-  console.log('22222222222222222222222222222222222 ::'+request.body.keyword);
-}
-if(request.body){
-  console.log('33333333333333333333333333333333333 ::'+request.body['keyword']);
-}
+    console.log('11111111111111111111111111111111111 ::'+request.keyword);
+  }
+  if(request.body){
+    console.log('22222222222222222222222222222222222 ::'+request.body.keyword);
+  }
+  if(request.body){
+    console.log('33333333333333333333333333333333333 ::'+request.body.keyword);
+  }
   var queryParams = '/foodinfo/search.do?' + encodeURIComponent('uid') + '=' + encodeURIComponent('LQUV6MOX');
   queryParams += '&' + encodeURIComponent('w') + '=' + encodeURIComponent(user_keyword);
 
