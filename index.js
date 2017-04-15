c45bf0797a56fc3ab0952a517ab86a637258fe0e
popsapple <saysun34@gmail.com>
2017-04-15 17:25:42 +0900

    ㅇ
    ㅇ

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ app.get('/', function(request, response)  {
   if(request.keyword){
     console.log('11111111111111111111111111111111111 ::'+request.keyword);
   }
-  if(request.body){
-    console.log('22222222222222222222222222222222222 ::'+request.params);
+  if(response.body){
+    console.log('22222222222222222222222222222222222 ::'+response.body.keyword);
   }
   if(request.body){
     console.log('33333333333333333333333333333333333 ::'+request.body.keyword);
