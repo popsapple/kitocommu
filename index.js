ac8f1639248f0a45a7254bb8e29d14f2cdc30499
popsapple <saysun34@gmail.com>
2017-04-15 17:32:54 +0900

    ㅇ
    ㅇ

--- index.js
+++ index.js
@@ -6,20 +6,21 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 
-
-
 app.get('/', function(request, response)  {
   var user_keyword;
   request.keyword ? user_keyword = request.keyword : user_keyword = '유기농';
   JSON.stringify(request.body);
+  request.header('Content-type','application/json');
+  request.header('Charset','utf8');
+
   if(request.keyword){
     console.log('11111111111111111111111111111111111 ::'+request.keyword);
   }
