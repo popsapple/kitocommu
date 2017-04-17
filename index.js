59cf8e3fec1358dba0bb25e095ce39cd5f270218
popsapple <saysun34@gmail.com>
2017-04-18 01:19:59 +0900

    dd

--- index.js
+++ index.js
@@ -15,20 +15,54 @@ app.use(bodyParser.json());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+function SearchFoodInfo(user_keyword){
 
+  var queryParams = '/foodinfo/search.do?' + encodeURIComponent('uid') + '=' + encodeURIComponent('LQUV6MOX');
+  queryParams += '&' + encodeURIComponent('w') + '=' + encodeURIComponent(user_keyword);
+
+  var opts = {
+    host: 'api.dbstore.or.kr',
+    path: queryParams,
+    port: '8880',
+    method: 'POST',
+    headers: {'x-waple-authorization': 'MzY4LTE0OTE4NDE3MDg3NzUtMjVkNzNiMmYtZjQ3Ni00OTRiLTk3M2ItMmZmNDc2Mjk0YmI5',
+    'content-type' : 'application/x-www-form-urlencoded; charset=UTF-8'}
+  };
+
+  var str = '';
+  callback = function(res) {
+    res.on('data', function (chunk) {
+      str += chunk;
+    });
+
+    res.on('end', function () {
+      //parse forecast.io message
+      var data = JSON.parse(str);
+      // merge res.locals
+      opts._locals = response.locals;
+
+      response.render('pages/index', data);
+    });
+  }
+
+  var req = data_respons.request(opts, callback);
+
+  req.on('error', function(e) {
+    console.log('ERROR: ' + e.message);
+  });
+
+  req.end();
+}
 
 app.get('/', function(request, response) {
+  var user_keyword = '유기농';
+  SearchFoodInfo(user_keyword);
   response.render('pages/index');
 });
 
 app.post('/', function(request, response) {
-  var user_keyword = '유기농';
-
-  if(request.body) {
-    console.log("DATA from AJAX = " + request.body.name);//This was the solution
-  }
-
-
+  var user_keyword = request.body.name;
+  SearchFoodInfo(user_keyword);
   response.render('pages/index');
 });
 
