13629b3771357c6504f596fcf0409bcc077ea082
popsapple <saysun34@gmail.com>
2017-04-17 22:38:37 +0900

    에러수정

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var data_respons = require('http');
 var bodyParser = require('body-parser');
+var bodyParserJsonError = require('express-body-parser-json-error');
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 
@@ -11,52 +12,63 @@ app.set('view engine', 'ejs');
 app.use(express.bodyParser());
 
 app.get('/', function(request, response) {
+  var user_keyword = '유기농';
 
-    if(request.body) {
-      console.log("11111111111111111111 ::"+request.body.keyword_item);
-    }
-    var user_keyword = '유기농';
+  if(request.body) {
+    console.log("11111111111111111111 ::"+request.body.keyword_item);
+  }
 
-    var queryParams = '/foodinfo/search.do?' + encodeURIComponent('uid') + '=' + encodeURIComponent('LQUV6MOX');
-    queryParams += '&' + encodeURIComponent('w') + '=' + encodeURIComponent(user_keyword);
+  var queryParams = '/foodinfo/search.do?' + encodeURIComponent('uid') + '=' + encodeURIComponent('LQUV6MOX');
+  queryParams += '&' + encodeURIComponent('w') + '=' + encodeURIComponent(user_keyword);
 
-    var opts = {
-      host: 'api.dbstore.or.kr',
-      path: queryParams,
-      port: '8880',
-      method: 'POST',
-      headers: {'x-waple-authorization': 'MzY4LTE0OTE4NDE3MDg3NzUtMjVkNzNiMmYtZjQ3Ni00OTRiLTk3M2ItMmZmNDc2Mjk0YmI5',
-      'content-type' : 'application/x-www-form-urlencoded; charset=UTF-8'}
-    };
+  var opts = {
+    host: 'api.dbstore.or.kr',
+    path: queryParams,
+    port: '8880',
+    method: 'POST',
+    headers: {'x-waple-authorization': 'MzY4LTE0OTE4NDE3MDg3NzUtMjVkNzNiMmYtZjQ3Ni00OTRiLTk3M2ItMmZmNDc2Mjk0YmI5',
+    'content-type' : 'application/x-www-form-urlencoded; charset=UTF-8'}
+  };
 
-    var str = '';
-    callback = function(res) {
-      res.on('data', function (chunk) {
-        str += chunk;
-      });
-
-      res.on('end', function () {
-        //parse forecast.io message
-        var data = JSON.parse(str);
-          console.log('sssss :============= ::'+data);
-        // merge res.locals
-        opts._locals = response.locals;
-
-        response.render('pages/index', data);
-      });
-    }
-
-    var req = data_respons.request(opts, callback);
-
-    req.on('error', function(e) {
-      console.log('ERROR: ' + e.message);
+  var str = '';
+  callback = function(res) {
+    res.on('data', function (chunk) {
+      str += chunk;
     });
 
-  req.end();
-  next();
+    res.on('end', function () {
+      //parse forecast.io message
+      var data = JSON.parse(str);
+      // merge res.locals
+      opts._locals = response.locals;
+
+      response.render('pages/index', data);
+    });
+  }
+
+  var req = data_respons.request(opts, callback);
+
+  req.on('error', function(e) {
+  console.log('ERROR: ' + e.message);
+
+  request.on('end', function () {
+    //parse forecast.io message
+    var data = JSON.parse(str);
+      console.log('sssss :============= ::'+data);
+    // merge res.locals
+    opts._locals = response.locals;
+
+    response.render('pages/index', data);
+  });
+
 });
 
-app.listen(app.get('/', function() {
+  req.end();
+  //next();
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
 
