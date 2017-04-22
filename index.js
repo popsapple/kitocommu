b6950e3b3a89738eade927aaa1a02fb2f6362a96
popsapple <saysun34@gmail.com>
2017-04-23 01:45:11 +0900

    인덱스 렌더 추가

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.use(bodyParser.json());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
