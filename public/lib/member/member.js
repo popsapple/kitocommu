94ec67a05c93f9dc106b65a2b4e2c8e7842cbe29
popsapple <saysun34@gmail.com>
2017-04-28 12:59:38 +0900

    회원가입쪽 DB 연결 및 처음 작성 쪽 부분 작업
    몽고 DB를 이용해 시그마를 만들고, 이 시그마를 통합적으로 관리하기 위해 SettingMemverDB 에다가 넣음. 차후
    Member 작업시 필요할 수 있기 때문에 Member은 Class화 해 둠.

--- public/lib/member/member.js
+++ public/lib/member/member.js
@@ -1,3 +1,42 @@
+function SettingMemberDB(){
+  var Memberschema = new Schema({
+    id:    String,
+    password:  Buffer,
+    nickname:  String,
+    email:  String,
+    tel:  Number,
+    sex:  String,
+    height:  Number,
+    weight:  Number,
+    writed: { type: Date, default: Date.now },
+    updated: { type: Date, default: Date.now }
+  });
+
+  var member_info = mongoose.model('member', Memberschema);
+
+  return member_info; // Member 안에 들어갈 DB 내용을 정의하고 리턴시킨다.
+}
+
+Member =  new Object(); // Member란 전부를 한꺼번에 가진 정의.
+Member.join = function(info,data,request){
+  for(var key in info){ // 값이 들어온 만큼...
+    data[key] = info[key];
+  }
+  data.writed = new Date();
+  data.updated = new Date();
+
+  data.save(function(err){
+      if(err){
+          console.error(err);
+          request.json({result: 0});
+          return;
+      }
+
+      response.render('member/join_member_step3');
+
+  });
+}
+
 module.exports.member = function (app) {
   app.get('/join_member_step1', function(request, response) {
     response.render('member/join_member_step1');
@@ -6,6 +45,7 @@ module.exports.member = function (app) {
   response.render('member/join_member_step2');
   });
   app.get('/join_member_step3', function(request, response) {
-  response.render('member/join_member_step3');
+//  SettingMemberDB(); // 시그마 정의
+  Member.join(request.query,SettingMemberDB(),request);
   });
 };
