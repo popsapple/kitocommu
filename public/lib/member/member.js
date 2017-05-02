4def1e6d4172e3262c8a18a918e8b2601c0532e6
popsapple <saysun34@gmail.com>
2017-05-02 16:26:12 +0900

    ㅇ

--- public/lib/member/member.js
+++ public/lib/member/member.js
@@ -183,8 +183,8 @@ Member.join = function(info,data,request,response,mongoose,type){
         }
       }
 
-      member.hash = member.makingHash(); // 사용자정의 메소드 호출
-      member.password = member.encryptPassword(info['pw']); // 사용자정의 메소드 호출
+      member.hash = this.makingHash(); // 사용자정의 메소드 호출
+      member.password = this.encryptPassword(info['pw']); // 사용자정의 메소드 호출
 
       member.save(function(err){
         if(err){
