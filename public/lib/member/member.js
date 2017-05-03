117a13ae4214ce93abf18497d11452a64c2e3db4
popsapple <saysun34@gmail.com>
2017-05-03 20:22:18 +0900

    ㅇ

--- public/lib/member/member.js
+++ public/lib/member/member.js
@@ -107,11 +107,7 @@ function MemberDB(mongoose,type,request,response){
   .get(function() { return this.password; });
 
   var MemberInfo; // 몽구스를 기존에 정의도니 schmea 가 있을 경우 overwrtie가 안 되기 때문에 에러처리가 필요하다
-  try {
-    MemberInfo = mongoose.model('member', Memberschema);
-  } catch (error) {
-    MemberInfo = mongoose.model('member');
-  }
+  MemberInfo = mongoose.model('member', Memberschema);
 
   if (type == 'login'){ // 로그인할때
     MemberInfo.findOne({id: request_list.id}, function(err, member){
