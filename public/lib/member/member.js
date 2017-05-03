2205f031b8cd48af3408ebe28218dd9f9a9a1ad0
popsapple <saysun34@gmail.com>
2017-05-03 23:33:35 +0900

    ㅇ

--- public/lib/member/member.js
+++ public/lib/member/member.js
@@ -68,13 +68,13 @@ function MemberDB(mongoose,type,request,response){
 
   // 비밀번호 암호화저장
   // hash 값
-  Memberschema.methods.makingHash = function(){
+  Memberschema.makingHash = function(){
     var dump = Math.round(new Date().valueOf()*Math.random());
     return dump;
   };
 
   // 비밀번호 암호화
-  Memberschema.methods.encryptPassword = function(pw,isHash){
+  Memberschema.encryptPassword = function(pw,isHash){
     var dump = pw;
     var shasum;
     // Hash가 아닌 Salt 인데... 이걸 치는 이유는 특정한 패턴의 비밀번호를 입력했을 때 해킹당하지 않게끔
@@ -91,7 +91,7 @@ function MemberDB(mongoose,type,request,response){
   };
 
   // 비밀번호 체크 시 사용
-  Memberschema.methods.checkloginPassword = function(pw_text,pw){
+  Memberschema.checkloginPassword = function(pw_text,pw){
     var is_true = false;
     var input = this.encryptPassword(pw_text,this.hash);
     input == pw ? is_true = true : is_true = false ;
