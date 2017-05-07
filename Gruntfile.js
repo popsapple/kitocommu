0551ff8c57e49a4bebf8140ebc86639c5c65ecb7
popsapple <saysun34@gmail.com>
2017-05-08 02:04:24 +0900

    ㅇ

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,60 @@
-/*global module:false*/
 module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    concat:{
+       options: {
+           banner: '/ 작업자 :: 현가람, 최종작업일 :: <%= grunt.template.today("yyyy-mm-dd") %> / ',  // 합치고 맨 처음에 출력할 내용
+           footer: '/ Keep his decrees and commands, which I am giving you today, so that it may go well with you and your children after you and that you may live long in the land the LORD your God gives you for all time.  / ',  // 합치고 맨 나중에 출력할 내용
+           separator: '/ separate /', // 각 파일이 합쳐지는 시점에 들어갈 내용
+           stripBanners:  { //각각의 파일에 쓰여있는 JavaScript banner comments 제거여부
+               force: false,
+               all: false
+           }
+       },
+       basic: {
+           src: [
+           'public/js/common/form_check.js'
+         ],
+         dest: 'public/result.js'
+       }
+   },
+   uglify:{
+     options: {
+        mangle: false, // 변수와 함수명 유지
+        compress: {
+            dropconsole: true // 콘솔 제거
+        },
+        beautify: true, // 코드 문법 유지
+        preserveComments: false // 모든 주석 제거
+    },
+    build: {
+        src: 'public/result.js',
+        dest: 'public/result.min.js'
+      }
+   },
+   cssmin:{
+     options: { shorthandCompacting: false, roundingPrecision: -1 },
+     target: {
+        files: [{
+          expand: true,
+          cwd: 'public/stylesheets',
+          src: ['*.css', '!*.min.css'],
+          dest: 'public/stylesheets',
+          ext: '.min.css'
+        }]
+      }
+    }
+
   });
 
   // These plugins provide necessary tasks.{% if (min_concat) { %}
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task(s).
-  grunt.registerTask('default');
-};
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
+
+}
