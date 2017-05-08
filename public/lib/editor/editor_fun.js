6809ebdf9ebada9ecac45d8a0d3346dd3d8ad6b0
popsapple <saysun34@gmail.com>
2017-05-09 04:19:52 +0900

    업로드시에만 bodyparser 가 변경되게(?)

--- public/lib/editor/editor_fun.js
+++ public/lib/editor/editor_fun.js
@@ -19,15 +19,10 @@ exports = module.exports = { UploadFile : function (app,aws,multer,multerS3,fs){
     })
     console.log("SETP02 ::");
     app.post('/upload', upload.single('upload'), function(req, res, next) {
-      /*var html;
-      html = "";
-      html += "<script type='text/javascript'>";
-      html += " var funcNum = " + req.query.CKEditorFuncNum + ";";
-      html += " var url = \"/images/" + fileName + "\";";
-      html += " var message = \"업로드 완료\";";
-      html += " window.parent.CKEDITOR.tools.callFunction(funcNum, url);";
-      html += "</script>";
-      res.send(html);*/
+      app.configure(function(){
+        app.use(express.methodOverride());
+        app.use(express.bodyParser({keepExtensions:true,uploadDir:path.join(__dirname,'/files'}));
+      });
 
       var filePath = req.file.path;
       var fileName = req.file.filename;
