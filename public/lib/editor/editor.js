1191ffe483c670b3f71933a80f545fe246b7f751
popsapple <saysun34@gmail.com>
2017-05-08 17:58:56 +0900

    ㅇ

--- public/lib/editor/editor.js
+++ public/lib/editor/editor.js
@@ -2,7 +2,7 @@ module.exports.editor_con = function(app,aws,multer,multerS3,fs){
   global.EDITOR_FUNCTION = require('./editor_fun.js');
   var UploadFile = new global.EDITOR_FUNCTION.UploadFile(app,aws,multer,multerS3,fs);
 
-  app.get('/board/write?board_id=MemberIntroduce', function(request, response) {
+  app.get('/board/write', function(request, response) {
     //MemberIntroduce
     var data ='';
     response.render('board/write', data);
