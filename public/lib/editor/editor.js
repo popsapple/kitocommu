36e3468aea4fd709b0b62d6286029a57320e60ef
popsapple <saysun34@gmail.com>
2017-05-08 15:54:04 +0900

    경로수정

--- public/lib/editor/editor.js
+++ public/lib/editor/editor.js
@@ -1,4 +1,4 @@
 exports = module.exports = {
-  global.EDITOR_FUNCTION = require('./public/lib/editor/editor_fun.js');
+  global.EDITOR_FUNCTION = require('./editor_fun.js');
   var UploadFile = new global.EDITOR_FUNCTION.UploadFile();
 }
