045223c29aa988cc9bff7a9b24f8852f942dc1bf
popsapple <saysun34@gmail.com>
2017-05-08 17:46:16 +0900

    ㅇ

--- public/lib/editor/editor_fun.js
+++ public/lib/editor/editor_fun.js
@@ -1,14 +1,14 @@
 exports = module.exports = { UploadFile : function (app,multer,multerS3,fs){
     //var uploadSetting = multer({dest:"../upload"});
     var s3 = new aws.S3({});
-
+  console.log("SETP01 ::");
     var upload = multer({
       storage: multerS3({
         s3: s3,
         bucket: 'kitocommu'
       })
     })
-
+    console.log("SETP02 ::");
     app.post('/upload', upload.single('upload'), function(req, res, next) {
       /*var html;
       html = "";
@@ -20,23 +20,18 @@ exports = module.exports = { UploadFile : function (app,multer,multerS3,fs){
       html += "</script>";
       res.send(html);*/
 
-      var tmpPath = req.file.path;
+      var filePath = req.file.path;
       var fileName = req.file.filename;
-      var newPath = "../public/images/" + fileName;
-      fs.rename(tmpPath, newPath, function (err) {
-        if (err) {
-          console.log(err);
-        }
-        var html;
-        html = "";
-        html += "<script type='text/javascript'>";
-        html += " var funcNum = " + req.query.CKEditorFuncNum + ";";
-        html += " var url = \"/images/" + fileName + "\";";
-        html += " var message = \"업로드 완료\";";
-        html += " window.parent.CKEDITOR.tools.callFunction(funcNum, url);";
-        html += "</script>";
-        res.send(html);
-      });
+      console.log("SETP03 ::"+filePath);
+      var html;
+      html = "";
+      html += "<script type='text/javascript'>";
+      html += " var funcNum = " + req.query.CKEditorFuncNum + ";";
+      html += " var url ="+filePath+"/"+fileName;
+      html += " var message = \"업로드 완료\";";
+      html += " window.parent.CKEDITOR.tools.callFunction(funcNum, url);";
+      html += "</script>";
+      res.send(html);
     });
   }/*,
   MemberDbSetting  : function (){
