dd86dedcf210136ed2faa69a8cebc459c24905bf
popsapple <saysun34@gmail.com>
2017-05-08 17:55:56 +0900

    쓰기페이지 연결

--- public/lib/editor/editor.js
+++ public/lib/editor/editor.js
@@ -1,4 +1,9 @@
 module.exports.editor_con = function(app,aws,multer,multerS3,fs){
   global.EDITOR_FUNCTION = require('./editor_fun.js');
   var UploadFile = new global.EDITOR_FUNCTION.UploadFile(app,aws,multer,multerS3,fs);
+
+  app.get('/board/write?board_id=MemberIntroduce', function(request, response) {
+    //MemberIntroduce
+    response.render('board/write');
+  });
 }
