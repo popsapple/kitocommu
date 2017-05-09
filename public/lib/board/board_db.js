b951dbd54c9273595da1fceb1384d138bd7aa160
popsapple <saysun34@gmail.com>
2017-05-09 20:46:45 +0900

    수정

--- public/lib/board/board_db.js
+++ public/lib/board/board_db.js
@@ -1,7 +1,4 @@
-exports = module.exports = { BoardMethod : function (obj,mongoose,request,response){
-    /** 나중에 스팸방지 달 때 필요할지 모르지 일단 넣어두자  **/
-  },
-  BoardDbSetting  : function (mongoose,request,response){
+exports = module.exports = {BoardDbSetting  : function (mongoose,request,response){
     console.log("들어옵니다");
     var obj = this;
     var Schema = mongoose.Schema;
