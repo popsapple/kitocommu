a2a51847eca40f1a971b67bb40547b094182d323
popsapple <saysun34@gmail.com>
2017-05-10 03:21:41 +0900

    index 라는 key 자체가 예약어(?) 라서 false 값인 0이 자꾸 들어갔던 걸지도..

--- public/lib/board/board.js
+++ public/lib/board/board.js
@@ -5,7 +5,7 @@ Board.write = function(info,request,response,mongoose,collection){
   save_data = global.BOARD_DB.model;
   save_data = new save_data(save_data.schema);
 
-  save_data.index = 0;
+  save_data.post_index = 0;
   save_data.reply = "";
   save_data.category = info.category;
   save_data.is_notice = info.is_notice;
@@ -28,7 +28,7 @@ Board.write = function(info,request,response,mongoose,collection){
         console.log("에러입니다");
         return;
     }
-    console.log("STEP 05 ::"+save_data.index);
+    console.log("STEP 05 ::"+save_data.post_index);
     response.render('board/write_ok',save_data);
   });
 }
--- public/lib/board/board_db.js
+++ public/lib/board/board_db.js
@@ -3,7 +3,7 @@ exports = module.exports = {BoardDbSetting  : function (mongoose,request,respons
     var Schema = mongoose.Schema;
 
     var Memberschema = new Schema({
-      index: Number,
+      post_index: Number,
       reply: String,
       writed:  { type: Date, default: Date.now },
       category:  String,
@@ -25,16 +25,16 @@ exports = module.exports = {BoardDbSetting  : function (mongoose,request,respons
     if (type == 'save'){
       var that = this;
       function settingIndex() {
-        BOARD_DB_MODEL.findOne({index: obj.index}, function(err, board){
+        BOARD_DB_MODEL.findOne({post_index: obj.post_index}, function(err, board){
           if(err) {
             return false;
           }
           if(board){
-            console.log("인덱스값 증가됩니다 ::"+obj.index);
-            obj.index += 1;
+            console.log("인덱스값 증가됩니다 ::"+obj.post_index);
+            obj.post_index += 1;
             setTimeout(settingIndex(),50);
           } else {
-            console.log("인덱스값을 그대로 내보냅니다 ::"+obj.index);
+            console.log("인덱스값을 그대로 내보냅니다 ::"+obj.post_index);
           //  console.log("인덱스값을 그대로 내보냅니다 ::"+board._index);
             return false;
           }
