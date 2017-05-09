6a9237c2569a67319f9d94f7463a9898e88b5700
popsapple <saysun34@gmail.com>
2017-05-09 19:08:54 +0900

    ㅇ

--- public/lib/board/board_db.js
+++ public/lib/board/board_db.js
@@ -2,6 +2,7 @@ exports = module.exports = { BoardrMethod : function (obj,mongoose,request,respo
     /** 나중에 스팸방지 달 때 필요할지 모르지 일단 넣어두자  **/
   },
   BoardDbSetting  : function (mongoose,request,response){
+    console.log("들어옵니다");
     var obj = this;
     var Schema = mongoose.Schema;
 
