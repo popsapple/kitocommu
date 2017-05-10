7ffdfb807b17ad440988974981a6ea0eb4f24e57
popsapple <saysun34@gmail.com>
2017-05-11 01:36:27 +0900

    ㅇ

--- public/lib/board/board_db.js
+++ public/lib/board/board_db.js
@@ -61,10 +61,23 @@ exports = module.exports = {BoardDbSetting  : function (mongoose,request,respons
         pageOfCount[i] = i;
       }
       var renderOfCount = pageOfCount.slice(0); // slice 를 이용한 깊은 복사.
-      if(page_num_ <= (page_length_/2)){
-        obj.board_paging = renderOfCount.slice(0,page_length_);
+      this.getCountArray = function(countarray,type){
+        var i = 0;
+        if(type == 'all'){
+          countarray = renderOfCount.slice(0,(page_length_-1));
+          return false;
+        }
+        for(var j = page_num_-4; j > (page_num_+5); j++){
+          if(renderOfCount[j]){
+            countarray[i] = j;
+          }
+          i++;
+        }
+      };
+      if(page_num_ <= ((page_length_-1)/2)){
+        this.getCountArray(obj.board_paging,'all');
       }else{
-        obj.board_paging = renderOfCount.slice((page_num_-4),(page_num_+5));
+        this.getCountArray(obj.board_paging); //renderOfCount.slice((page_num_-4),(page_num_+5));
       }
       response.render('board/list',obj);
     });
