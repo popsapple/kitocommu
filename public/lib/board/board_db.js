d5767c53dde817ddb0c80d95c005f3e598206d17
popsapple <saysun34@gmail.com>
2017-05-10 15:13:01 +0900

    ㅇ

--- public/lib/board/board_db.js
+++ public/lib/board/board_db.js
@@ -23,9 +23,10 @@ exports = module.exports = {BoardDbSetting  : function (mongoose,request,respons
 
     var BOARD_DB_MODEL = global.BOARD_DB.model;
     if (type == 'save'){
-      console.log("STEP02 ::::");
+
       BOARD_DB_MODEL.count({}, function(error, numOfDocs){
           obj.post_index = numOfDocs;
+          console.log("STEP02 ::::"+obj.post_index);
       });
 
       // 동기적으로 실행해야 하므로 콜백으로 처리한 함수.
