c56e97948263887c25a5060376eca3a9cd8272b7
popsapple <saysun34@gmail.com>
2017-05-12 21:59:30 +0900

    일단 원래대로 돌리기

--- public/lib/board/board_db0.js
+++ public/lib/board/board_db0.js
@@ -65,6 +65,7 @@ exports = module.exports = {BoardDbSetting  : function (mongoose,request,respons
     BOARD_DB_MODEL.find(search_hint, function(err, board){
       data.board_list = board;
       obj.board_post_length = data.board_list.length;
+      console.log("DDDDDDDDDDDDDD ::: "+obj.board_post_length);
       data.board_list = data.board_list.slice(page_num,page_length);
       data.page_ = request.query.page;
       data.searchoption = search_option;
