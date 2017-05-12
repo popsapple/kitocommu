40d86a8ccb35e5320332f6e5651937b336a2e66b
popsapple <saysun34@gmail.com>
2017-05-12 15:56:24 +0900

    페이징기능

--- public/lib/board/board.js
+++ public/lib/board/board.js
@@ -33,7 +33,7 @@ Board.write = function(info,request,response,mongoose,collection){
 
 Board.list_render = function(info,request,response,mongoose,collection){
   function PagingFunction(obj,mongoose,request,response){
-    var read_data_ = new global.BOARD_DB.getBoardPagingByIndex(obj,mongoose,request,response);
+    var read_data_ = new global.BOARD_DB.getBoardPagingByIndex(obj,mongoose,request,response,'search');
   }
   var read_data = new global.BOARD_DB.BoardDbSetting(mongoose,request,response,collection);
   var read_data_ = new global.BOARD_DB.getBoardListByIndex(read_data,mongoose,request,response,function(obj,mongoose,request,response){
