685edcdb4c553f161bf4a43c4c7470bb83b14279
popsapple <saysun34@gmail.com>
2017-05-13 03:13:01 +0900

    ㅇ

--- public/lib/board/board_db.js
+++ public/lib/board/board_db.js
@@ -91,7 +91,7 @@ exports = module.exports = {BoardDbSetting  : function (mongoose,request,respons
     var page_num = request.query.post_index;
     var page_num_ = parseInt(page_num)+1;
     var board_id = request.query.board_table_id;
-    console.log("===========");
+    console.log("=========== remove ::"+board_id);
     BOARD_DB_MODEL.remove({post_index: page_num}, function(err,board){
       BOARD_DB_MODEL.update({$max: {post_index: page_num}}, {$inc: {post_index: -1}}, {multi: true});
       response.redirect("/board/list?board_table_id="+board_id+"&page=0&page_length=10");
