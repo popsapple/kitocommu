902650f94a6d89d3e7d8d1ce355c97809128f6cd
popsapple <saysun34@gmail.com>
2017-05-13 17:28:18 +0900

    d

--- public/lib/board/board.js
+++ public/lib/board/board.js
@@ -95,7 +95,7 @@ module.exports.board_con = function(app,mongoose){
   });
 
   app.get('/board/write', function(request, response) {
-    if(request.body && request.query.post_index){
+    if(request.query && request.query.post_index){
       console.log("============ 01===========");
       Board.view(request.query,request,response,mongoose,board_id,'modify');
     }else{
