caeac70091e593dd0b37ac2d1a9aaf8186b6d833
popsapple <saysun34@gmail.com>
2017-05-16 01:32:20 +0900

    버튼클릭시 함수 실행되게끔

--- public/js/common/board.js
+++ public/js/common/board.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
-  $('#ajaxform').ajaxForm({
-    url: "/upload_thumnail",
-    enctype: "multipart/form-data", // 여기에 url과 enctype은 꼭 지정해주어야 하는 부분이며 multipart로 지정해주지 않으면 controller로 파일을 보낼 수 없음
-    success: function(result){
-      alert(result);
-    }
-   });
+  $('#BoardThumnailButton').on('click',function(){
+    $('#ajaxform').ajaxForm({
+      url: "/upload_thumnail",
+      enctype: "multipart/form-data", // 여기에 url과 enctype은 꼭 지정해주어야 하는 부분이며 multipart로 지정해주지 않으면 controller로 파일을 보낼 수 없음
+      success: function(result){
+        alert(result);
+      }
+     });
 
-   $("#ajaxform").submit();
-  //onSearchPost($("#BoardSearchOption"),$("#BoardSearchValue"),$("#boardList"),$("#boardPaging"));
-  //$("#joinSex").attr("data-sex") ? $("#joinSex").val($("#joinSex").attr("data-sex")) : '';
+     $("#ajaxform").submit();
+  });
 });
