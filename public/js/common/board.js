1509c46715bdadfcbb2b6f42642c3980cced5077
popsapple <saysun34@gmail.com>
2017-05-16 01:28:50 +0900

    ㅇ

--- public/js/common/board.js
+++ public/js/common/board.js
@@ -1,13 +1,9 @@
 $(document).ready(function(){
   $('#ajaxform').ajaxForm({
-    beforeSubmit: function (data, frm, opt) {
-      return true;
-    },
-    success: function(data, statusText){
-      $("#BoardThumnailValue").val(data);
-    },
-    error: function(){
-       alert("파일 전송시 에러가 발생했습니다");
+    url: "/upload_thumnail",
+    enctype: "multipart/form-data", // 여기에 url과 enctype은 꼭 지정해주어야 하는 부분이며 multipart로 지정해주지 않으면 controller로 파일을 보낼 수 없음
+    success: function(result){
+      alert(result);
     }
    });
 
