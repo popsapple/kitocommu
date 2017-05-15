05cc2890221da23e05ae38aa7f205ea446e7e16a
popsapple <saysun34@gmail.com>
2017-05-15 15:21:12 +0900

    ㅇ

--- public/lib/board/board_db.js
+++ public/lib/board/board_db.js
@@ -33,13 +33,14 @@ exports = module.exports = {BoardDbSetting  : function (mongoose,request,respons
     mongoose.modelSchemas = {};
     var config = {};
     var BOARD_STYLE_MODEL = mongoose.model('board_type_list', BoardConfigSchema);
-    BOARD_STYLE_MODEL.findOne({board: board_id}, function(err,board_config){
+    BOARD_STYLE_MODEL.find({board: board_id}, function(err,board_config){
       for (var key in board_config){
         console.log("????????????? :: "+key+" :: ?????????"+board_config[key]);
-      }
+      }/*
       config.list_type = board_config.list_type;
       config.css_file = board_config.css_file;
       callback(config);
+      */
     });
   },getBoardLastIndex : function (obj,mongoose,request,response,callback){
 
