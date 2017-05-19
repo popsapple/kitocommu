ea1c4a17e1b0f0df4d41bd0b60a985edb14ec69b
popsapple <saysun34@gmail.com>
2017-05-20 03:01:28 +0900

    ㅇ

--- public/js/ckeditor/plugins/responsivness/dialogs/beResponsive.js
+++ public/js/ckeditor/plugins/responsivness/dialogs/beResponsive.js
@@ -1,34 +1,11 @@
 /**
  * @file
- * Plugin definition and settings.
+ * Plugin functions.
  */
-'use strict';
 
-CKEDITOR.plugins.add('responsivearea', {
-  icons: 'responsivearea',
-  init: pluginInit
-});
-
-function pluginInit(editor) {
-  editor.responsivearea_path =  this.path;
-
-  editor.ui.addButton('AddResponsiveArea', {
-    label: Drupal.t('Insert a responsive area'),
-    command: 'beResponsive',
-    icon: this.path + 'icons/responsivearea.png'
-  });
-
-
-  editor.addCommand('beResponsive', {
-    exec: function (editor, data) {
-      editor.openDialog('AddResponsiveAreaDialog' + editor.name);
-    }
-  });
-
-  CKEDITOR.dialog.add('AddResponsiveAreaDialog' + editor.name,beResponsive);
-}
-
-function beResponsive(editor,data) {
+CKEDITOR.dialog.add('beResponsive', function (editor) {
+  'use strict';
+  var path = Drupal.settings.ckeditor_responsive_plugin.basePath;
   return {
     title: 'Responsive Areas',
     minWidth: 400,
@@ -43,11 +20,11 @@ function beResponsive(editor,data) {
             id: 'layout',
             label: '<p><strong>Please choose the layout you want</strong></p><br />',
             items: [
-              ['<br /><em>1 area - 100%</em><br /><br /><img src="' + editor.responsivearea_path + '/images/1_100.png" />', '1_100'],
-              ['<br /><em>2 areas - 50% - 50%</em><br /><br /><img src="' + editor.responsivearea_path + '/images/2_50_50.png" />', '2_50_50'],
-              ['<br /><em>2 areas - 75% - 25%</em><br /><br /><img src="' + editor.responsivearea_path + '/images/2_75_25.png" />', '2_75_25'],
-              ['<br /><em>2 areas - 25% - 75%</em><br /><br /><img src="' + editor.responsivearea_path + '/images/2_25_75.png" />', '2_25_75'],
-              ['<br /><em>3 areas - 33% - 34% - 33%</em><br /><br /><img src="' + editor.responsivearea_path + '/images/3_33_34_33.png" />', '3_33_34_33']
+              ['<br /><em>1 area - 100%</em><br /><br /><img src="' + path + '/responsivness/images/1_100.png" />', '1_100'],
+              ['<br /><em>2 areas - 50% - 50%</em><br /><br /><img src="' + path + '/responsivness/images/2_50_50.png" />', '2_50_50'],
+              ['<br /><em>2 areas - 75% - 25%</em><br /><br /><img src="' + path + '/responsivness/images/2_75_25.png" />', '2_75_25'],
+              ['<br /><em>2 areas - 25% - 75%</em><br /><br /><img src="' + path + '/responsivness/images/2_25_75.png" />', '2_25_75'],
+              ['<br /><em>3 areas - 33% - 34% - 33%</em><br /><br /><img src="' + path + '/responsivness/images/3_33_34_33.png" />', '3_33_34_33']
             ],
             style: 'display: block;text-align:center',
             default: '2_50_50'
@@ -59,16 +36,16 @@ function beResponsive(editor,data) {
       var dialog = this;
       var mode = dialog.getValueOf('tab-basic', 'layout');
       var tpl = responsiveness_get_template(mode);
-      if (tpl !== "") {
+      if (tpl !== '') {
         editor.insertHtml(tpl);
       }
     }
   };
-}
+});
 
 function responsiveness_get_template(tpl) {
   'use strict';
-  var grid = "";
+  var grid = '';
   switch (tpl) {
     case '1_100':
       grid = '<div class="ckeditor-col-container clearfix">';
@@ -104,7 +81,6 @@ function responsiveness_get_template(tpl) {
       grid += '<div class="grid-4 fourcol last-col"><p>lorem ipsum</p></div>';
       grid += '</div><br />';
       break;
-
   }
   return grid;
 }
