9ec0f2f49f91016614f7920d9d3f93aae1cbed87
popsapple <saysun34@gmail.com>
2017-05-22 01:23:21 +0900

    리터럴 문제.
    생성자를 이용한 패턴을 뽑아내는 예제들 자체가  없음.... 아마 리터럴 방식으로 써야지 맞나 봄.

--- public/lib/editor/editor_fun.js
+++ public/lib/editor/editor_fun.js
@@ -114,7 +114,8 @@ exports = module.exports = { UploadFile : function (app,aws,multer,multerS3,fs){
       var removing = setTimeout(function(){
         for(var key in remove_item){
           console.log("어떠한 파일이 삭제되는가0000 :: "+remove_item[key]);
-          var pattern = new RegExp("(\/{1}(\w+))", "g");
+          //var pattern = new RegExp("(\/{1}(\w+))", "g");
+          var pattern = /(\/{1}(\w+))/g;
           var remove_item_key = pattern.exec(remove_item[key]);
           console.log("어떠한 파일이 삭제되는가0101 :: "+remove_item_key);
           remove_item_key = remove_item_key[1];
