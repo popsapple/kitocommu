9588ab0ceafe293dcee1612a280ec3149b5ad840
popsapple <saysun34@gmail.com>
2017-05-22 01:39:04 +0900

    ㅇ

--- public/lib/editor/editor_fun.js
+++ public/lib/editor/editor_fun.js
@@ -115,16 +115,14 @@ exports = module.exports = { UploadFile : function (app,aws,multer,multerS3,fs){
         for(var key in remove_item){
           console.log("어떠한 파일이 삭제되는가0000 :: "+remove_item[key]);
           //var pattern = new RegExp("(\/{1}(\w+))", "g");
-          var pattern = /(\/{1}(\w+))/g;
+          var pattern = /(\/(\w+))/g;
           var remove_item_key = pattern.exec(remove_item[key]);
           console.log("어떠한 파일이 삭제되는가0101 :: "+remove_item_key);
           remove_item_key = remove_item_key[1];
 
           remove_item_key = remove_item_key.substring(1,remove_item_key.length);
           console.log("어떠한 파일이 삭제되는가0202 :: "+remove_item_key);
-          if(remove_item.hasOwnProterty(key)){
-            continue;
-          }
+
           (function(){
             console.log("함수 실행여부 체크");
             var params = {
