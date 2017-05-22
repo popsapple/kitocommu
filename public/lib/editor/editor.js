b8c20e2e282a43e94497bb3d5604f569b3935e09
popsapple <saysun34@gmail.com>
2017-05-22 15:40:58 +0900

    에디터 관련 기능 정리
    기존에 db 및 아마존 s3 관리하던 부분과 그걸 불러오는 부분을 떼어냄.

--- public/lib/editor/editor.js
+++ public/lib/editor/editor.js
@@ -1,4 +1,53 @@
 module.exports.editor_con = function(app,aws,multer,multerS3,fs){
-  global.EDITOR_FUNCTION = require('./editor_fun.js');
-  var UploadFile = new global.EDITOR_FUNCTION.UploadFile(app,aws,multer,multerS3,fs);
+  global.EDITOR_FUNCTION = require('./editor_db.js');
+
+  var obj = this;
+
+  obj.file_listing = function(req,filePath) { // 현재 작성중인 상태일 때 추가되는 첨부파일 리스트.
+
+    obj.filelist = req.session.filelist;
+
+    obj.filelist.push(filePath);
+    for(key in obj.filelist){
+      if(obj.filelist.hasOwnProperty(key)){
+        console.log("SESSION FILES LIST :: KEY ::"+key+" :: VALUE :: "+obj.filelist[key]);
+      }
+    }
+  };
+
+  aws.config.update({
+      secretAccessKey: 'gO/NS90rJJ/ZQSQsurEn2U9Tiqn3Af029PEFMMbl',
+      accessKeyId: 'AKIAI3NXS4PH4Y3ZWJ6A',
+      region: 'ap-northeast-2',
+      endpoint: 's3.ap-northeast-2.amazonaws.com',
+      signatureVersion: 'v4'
+  });
+
+  var s3 = new aws.S3();
+  var upload = multer({
+    storage: multerS3({
+      s3: s3,
+      bucket: 'kitocommu'
+    }),
+    onFileUploadStart: function (file) {
+      if (file.extension == 'exe' || file.extension == 'html' || file.extension == 'htm' || file.extension == 'php' || file.extension == 'php3' || file.extension == 'php4' || file.extension == 'phtml' || file.extension == 'phps' || file.extension == 'in' || file.extension == 'cgi' || file.extension == 'pi' || file.extension == 'shtml' || file.extension == 'jsp' || file.extension == 'asp' || file.extension == 'js') {
+        response.send("<script>alert('해당 확장자의 파일은 업로드하실 수 없습니다.');</script>");
+        return false;
+      };
+    }
+  });
+
+  var upload_callback = upload.single('upload');
+  app.post('/upload', function(req, res, next) {
+    var UploadFile = new global.EDITOR_FUNCTION.UploadFile(upload,s3,req,res,obj);
+  });
+
+  var thumnail_upload_callback = upload.single('thumnailfile'); // 실제 input에 있는 name이랑 이름이 같아야 함
+  app.post('/upload_thumnail', function(req, res, next) {
+    var UploadThumnailFile = new global.EDITOR_FUNCTION.UploadThumnailFile(thumnail_upload_callback,s3,req,res,obj);
+  });
+
+  app.post('/upload_file_delete', function(req, res, next) {
+    var FileDelete = new global.EDITOR_FUNCTION.FileDelete(s3,req,res);
+  });
 }
---
+++ public/lib/editor/editor_db.js
@@ -0,0 +1,82 @@
+exports = module.exports = { UploadFile : function (upload,s3,req,res,obj){
+      upload_callback(req, res, function (err) {
+        if (err) {
+          // 업로드 에러시
+          return;
+        }
+
+        var filePath = req.file.location;
+        obj.file_listing(req,filePath);  // 수정중인 첨부파일 리스트 편집.
+
+        var html;
+        html = "";
+        html += "<script type='text/javascript'>";
+        html += " var funcNum = " + req.query.CKEditorFuncNum + ";";
+        html += " var url ="+"\""+filePath+"\""+ ";";
+        html += " var message = \"업로드 완료\";";
+        html += " window.parent.CKEDITOR.tools.callFunction(funcNum, url);";
+        html += "</script>";
+        res.send(html);
+      });
+    }, UploadThumnailFile : function (thumnail_upload_callback,s3,req,res) {
+      thumnail_upload_callback(req, res, function (err) {
+        if (err) {
+          return;
+        }
+        var filePath;
+        for(var key in req.file){
+          filePath = req.file.location;
+        };
+
+        obj.file_listing(req,filePath);  // 수정중인 첨부파일 리스트 편집.
+
+        var html;
+        html ="\""+filePath+"\"";
+        res.send(html);
+      });
+    }, FileDelete : function (s3,req,res) {
+      var remove_item;
+      var post_idx;
+      var count = 0;
+      var RemovingFile = function() {
+        for(var key in remove_item){
+        (function(){
+          var pattern = /(\/(\w+))/g;
+          var remove_item_key = remove_item[key].match(pattern);
+          remove_item_key = remove_item_key[1];
+
+          remove_item_key = remove_item_key.substring(1,remove_item_key.length);
+
+          var params = {
+            Bucket: 'kitocommu',
+            Key: remove_item_key
+          };
+          s3.deleteObject(params, function(err, data) {
+            if (err) {
+              console.log("삭제가 안 되었음"+err+" :: "+err.stack); // 에러시 표시
+              return false;
+            }
+            else {
+
+            }
+          });
+          count++;
+        })();
+      }
+    };
+    if(req.body.is_remove_post == "writing"){
+      remove_item = req.session.filelist; // 작성중인걸 삭제할때
+      RemovingFile();
+    }
+    else{ // 이미 작성되어있는걸 삭제할때
+      var BOARD_DB_MODEL = global.BOARD_DB.model;
+      var BOARD_DB_MODEL_SCHEMA = global.BOARD_DB.model.schema.paths;
+      post_idx = req.body.post_index;
+
+      BOARD_DB_MODEL.findOne({post_index: post_idx}, function(err,board){
+        remove_item = board.file_list.split(',');
+        RemovingFile();
+      });
+    }
+  }
+}
