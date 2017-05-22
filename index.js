aca0f8942aa92e262a6bb9a7569a9b18b16757ea
popsapple <saysun34@gmail.com>
2017-05-22 18:06:17 +0900

    ㅇ

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ app.get('/', function(request, response, next) {
 });
 
 // 로그인 세션
+app.all('*', function(request, response, next) {
   console.log("GET SESSION NICKNAME1110101 ::"+request.session.nickname);
   global.MEMBERLIB.SettingSessionItem(app, request, response);
   next();
