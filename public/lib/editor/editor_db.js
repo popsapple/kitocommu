823e1fd90b510156de384ffa77c265562ccc6600
popsapple <saysun34@gmail.com>
2017-05-24 05:32:43 +0900

    첨부파일 없을 때 삭제

--- public/lib/editor/editor_db.js
+++ public/lib/editor/editor_db.js
@@ -74,7 +74,9 @@ exports = module.exports = { UploadFile : function (upload_callback,s3,req,res,o
       post_idx = req.body.post_index;
 
       BOARD_DB_MODEL.findOne({post_index: post_idx}, function(err,board){
-        remove_item = board.file_list.split(',');
+        if(board.file_list){
+          remove_item = board.file_list.split(',');
+        }
         RemovingFile();
       });
     }
