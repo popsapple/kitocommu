b7374853ba155a019cda720e9e0673bfb9ebb3db
popsapple <saysun34@gmail.com>
2017-05-24 02:21:13 +0900

    ㅇ

--- public/lib/board/board_db.js
+++ public/lib/board/board_db.js
@@ -151,7 +151,7 @@ exports = module.exports = {BoardDbSetting  : function (mongoose,request,respons
             global.MEMBERLIB.CheckAuthenfication(board_info_.comment_writer,request.session.userid,request,response,function(value_){
               board_info_.comments_list = comment;
               board_info_.is_comment_writer = value_;
-              console.log("작성자가 맞는지 확인 :: "+board_info_.is_comment_writer +" :: ");
+              console.log("작성자가 맞는지 확 인 :: "+board_info_.is_comment_writer +" :: ");
               response.render('board/view',board_info_);
             });
             var finded_count = db_object.find({post_index: post_index_}).count();
