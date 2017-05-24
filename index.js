fdf5930bc4413fa72c1c262cf57f8626997d4f20
popsapple <saysun34@gmail.com>
2017-05-24 20:00:09 +0900

    next 체크

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.all('*', function(request, response, next) {
   console.log("GET SESSION NICKNAME1110101 ::"+typeof(next));
 
   global.MEMBERLIB.SettingSessionItem(app, request, response);
-  if(typeof request.next === "function" || typeof req.next === "function"){
+  if(next && typeof(next) == "function"){
     next();
   }
 });
