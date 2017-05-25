2c64a4390afa05823211d387ee13e04c23826f9e
popsapple <saysun34@gmail.com>
2017-05-26 01:56:03 +0900

    -

--- public/lib/board/board_db.js
+++ public/lib/board/board_db.js
@@ -74,10 +74,9 @@ exports = module.exports = {BoardDbSetting  : function (mongoose,request,respons
             console.log("리플을 잘 찾아 오는지");
             var that_reply = {};
             var count = 0;
-            var max_post_length = page_num-page_length+1;
+            var max_post_length = page_num-page_length;
             that_reply.ReplyListing = function(count) {
               console.log("ReplyListing 실행 :: "+count);
-              var plus_count = count+1;
               if(count == max_post_length) {
                 return false;
               }else {
@@ -93,6 +92,8 @@ exports = module.exports = {BoardDbSetting  : function (mongoose,request,respons
                   data.board_list[count].reply_list.push(reply[count].reply_index);
                 }
               };
+
+              count++;
             };
 
             that_reply.ReplyListing(count);
