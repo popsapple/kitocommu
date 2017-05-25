4c2c73461ea3c43ce73cc2d46a229fc88cdd758e
popsapple <saysun34@gmail.com>
2017-05-26 01:43:21 +0900

    -

--- public/lib/board/board_db.js
+++ public/lib/board/board_db.js
@@ -72,16 +72,16 @@ exports = module.exports = {BoardDbSetting  : function (mongoose,request,respons
           callback(data,mongoose,request,response);
           that.db_reply_model.find({reply_index: { $gte: page_length, $lte: page_num }}, function(err, reply){
             console.log("리플을 잘 찾아 오는지");
-            var that = this;
+            var that_reply = this;
             var count = 0;
             var max_post_length = page_num-page_length+1;
-            that.ReplyListing = function(count){
+            that_reply.ReplyListing = function(count){
               console.log("ReplyListing 실행 :: "+count);
               var plus_count = count+1;
               if(count == max_post_length) {
                 return false;
               }else {
-                that.ReplyListing(plus_count);
+                that_reply.ReplyListing(plus_count);
               };
 
               if(data.board_list[count].post_index == reply[count].reply_index){
