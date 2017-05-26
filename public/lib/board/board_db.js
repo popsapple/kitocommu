5ca607f7068d7a2b82c5982b8b56acc2da21a73e
popsapple <saysun34@gmail.com>
2017-05-26 23:18:02 +0900

    -

--- public/lib/board/board_db.js
+++ public/lib/board/board_db.js
@@ -96,6 +96,7 @@ exports = module.exports = {BoardDbSetting  : function (mongoose,request,respons
                         console.log("ReplyPostListing  reply_count ::"+reply_count);
                         console.log("ReplyPostListing ::"+post_count);
                         if(data.board_list[post_count].post_index == reply_doc[reply_count].reply_index) {
+                          reply_doc[reply_count].board_table_id = "ReplyList";
                           data.board_list[post_count].reply_list.push(reply_doc[reply_count]);
                         }
                         if(post_count == max_post_length){
