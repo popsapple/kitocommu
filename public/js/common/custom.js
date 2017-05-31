f0ec20e2592c7b9d7dcb16033a34efda6bef664f
popsapple <saysun34@gmail.com>
2017-05-31 18:47:18 +0900

    슬라이더 추가

---
+++ public/js/common/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  if($('.main_visual.flexslider')){
+    $('.main_visual.flexslider').flexslider({
+      animation: "slide",
+      customDirectionNav : "#MainSliderButtons button"
+    });
+  }
+});
