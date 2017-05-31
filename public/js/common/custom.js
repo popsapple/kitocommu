2cfad0d1dd7b2e63894fc7f3659cae6990eca97e
popsapple <saysun34@gmail.com>
2017-06-01 02:09:52 +0900

    -

--- public/js/common/custom.js
+++ public/js/common/custom.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 
   if($(".designcustom .navbar-collapse")){
     $(".designcustom .navbar-collapse").on('click',function(event){
-      if($(event.target).hasClass("navbar-collapse")){
+      if($(event.target).hasClass("navbar-collapse") || $(event.target).hasClass("close_btn")){
         $(this).removeClass('in');
         return false;
       }
