7edb374ec6fd1a19b4f2ea545b83b37ae680db37
popsapple <saysun34@gmail.com>
2017-06-01 04:46:48 +0900

    슬라이더 작업 시작

--- public/js/common/custom.js
+++ public/js/common/custom.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   if($('.main_visual.flexslider')){
     $('.main_visual.flexslider').flexslider({
       animation: "slide",
-      customDirectionNav : "#MainSliderButtons button"
+      manualControls: "#MainSliderButtons button"
     });
   }
 
