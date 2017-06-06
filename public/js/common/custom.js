b0f716ce2ab0fca94e5cc9e396c27c782edb0a85
popsapple <saysun34@gmail.com>
2017-06-06 21:10:01 +0900

    -

--- public/js/common/custom.js
+++ public/js/common/custom.js
@@ -303,7 +303,7 @@ function LoadingPage(){
       },
       complete:function(){
         setTimeout(function(){
-          $('.loading_div').fadeOut(500);
+          //$('.loading_div').fadeOut(500);
         },2000);
       },
       error:function(e){
