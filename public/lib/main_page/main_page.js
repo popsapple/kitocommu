ffba65fd72b0b8c9d311598a911557368f9e711d
popsapple <saysun34@gmail.com>
2017-07-08 16:56:49 +0900

    파일추가

---
+++ public/lib/main_page/main_page.js
@@ -0,0 +1,16 @@
+exports = module.exports = {index_page  : function (app,mongoose) {
+    // 처음 접속
+    app.get('/', function(request, response, next) {
+      var data = {};
+      data.board_data = [];
+      global.BOARD_DB.getBoardData(data,request,response,mongoose,['title','contents'],3,'DiteFaq',function(data){
+        global.BOARD_DB.getBoardData(data,request,response,mongoose,['title','contents','writer_nickname'],3,'MemberDiary',function(data){
+          response.render('pages/index',data);
+          if(typeof next == "function"){
+            next();
+          }
+        });
+      });
+    });
+  }
+}
