739ef3c173382845af7188161ec606aa9f8d9c25
popsapple <saysun34@gmail.com>
2017-07-10 18:23:13 +0900

    사이트맵 수정

--- sitemap_builder.js
+++ sitemap_builder.js
@@ -14,40 +14,35 @@ module.exports.rss_builder = function (app,mongoose) {
     var url = doc.ele('url');
     var loc = url.ele('loc',{},'https://kitocommu.herokuapp.com');
     var lastmod = url.ele('lastmod',{},new Date());
-    //var changefreq = url.ele('changefreq',{},'always');
+    var changefreq = url.ele('changefreq',{},'always');
     var priority = url.ele('priority',{},'0.9');
-
+    /** 서브페이지인 경우는 이렇게 **/
+    /* var url = doc.ele('url');
+    var loc = url.ele('loc',{},'https://kitocommu.herokuapp.com'+arr['link']);
+    var changefreq = url.ele('changefreq',{},'always'); */
     global.BOARD_DB.getBoardData(data,request,response,mongoose,['writed'],100,'MemberDiary',function(data){
       data.board_data['Board_MemberDiary'].forEach(function(arr,index){
         var url = doc.ele('url');
         var loc = url.ele('loc',{},'https://kitocommu.herokuapp.com'+arr['link']);
-        var lastmod = url.ele('lastmod',{},arr['writed']);
-      //  var changefreq = url.ele('changefreq',{},'always');
-        var priority = url.ele('priority',{},'0.5');
+        var changefreq = url.ele('changefreq',{},'always');
       });
       global.BOARD_DB.getBoardData(data,request,response,mongoose,['writed'],100,'MemberIntroduce',function(data){
         data.board_data['Board_MemberIntroduce'].forEach(function(arr,index){
           var url = doc.ele('url');
           var loc = url.ele('loc',{},'https://kitocommu.herokuapp.com'+arr['link']);
-          var lastmod = url.ele('lastmod',{},arr['writed']);
-          //var changefreq = url.ele('changefreq',{},'always');
-          var priority = url.ele('priority',{},'0.5');
+          var changefreq = url.ele('changefreq',{},'always');
         });
         global.BOARD_DB.getBoardData(data,request,response,mongoose,['writed'],100,'DiteFaq',function(data){
           data.board_data['Board_DiteFaq'].forEach(function(arr,index){
             var url = doc.ele('url');
             var loc = url.ele('loc',{},'https://kitocommu.herokuapp.com'+arr['link']);
-            var lastmod = url.ele('lastmod',{},arr['writed']);
-          //  var changefreq = url.ele('changefreq',{},'always');
-            var priority = url.ele('priority',{},'0.5');
+            var changefreq = url.ele('changefreq',{},'always');
           });
           global.BOARD_DB.getBoardData(data,request,response,mongoose,['writed'],100,'RecipeIntroduce',function(data){
             data.board_data['Board_RecipeIntroduce'].forEach(function(arr,index){
               var url = doc.ele('url');
               var loc = url.ele('loc',{},'https://kitocommu.herokuapp.com'+arr['link']);
-              var lastmod = url.ele('lastmod',{},arr['writed']);
-              //var changefreq = url.ele('changefreq',{},'always');
-              var priority = url.ele('priority',{},'0.5');
+              var changefreq = url.ele('changefreq',{},'always');
             });
             return callback(that, now_date, request, response, next, doc.end({ pretty: true}));
           });
