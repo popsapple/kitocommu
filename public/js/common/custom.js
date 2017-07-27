ccb146792239dee504de71bcb56b0d5478097c26
popsapple <saysun34@gmail.com>
2017-07-28 04:41:53 +0900

    ,

--- public/js/common/custom.js
+++ public/js/common/custom.js
@@ -577,15 +577,15 @@ $(document).ready(function() {
 $('#navbar').on('shown.bs.collapse', function () {
   $('.gnb_navbar').attr('aria-hidden','false');
   var newHeight = $('body').height();
-  $('html .gnb_navbarcall').css('height',newHeight+'!important');
-  console.log("열림 :: "+$('html .gnb_navbarcall').css('height',newHeight));
+  $('html .gnb_navbarcall').css('height',newHeight+'px !important');
+  console.log("열림 :: ");
   console.log("열림 :: "+newHeight);
 });
 $('#navbar').on('hidden.bs.collapse', function () {
   $('.gnb_navbar').attr('aria-hidden','true');
   var newHeight = 0;
-  $('html .gnb_navbarcall').css('height',newHeight+'!important');
-  console.log("닫힘 :: "+$('html .gnb_navbarcall').height(newHeight));
+  $('html .gnb_navbarcall').css('height',newHeight+'px !important');
+  console.log("닫힘 :: ");
   console.log("닫힘 :: "+newHeight);
 });
 $(window).scroll(function(event) {
