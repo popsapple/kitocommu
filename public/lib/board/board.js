1e385ab7fd6d80165bfb6173716121c09d7ef1ad
hyeongaram <saysun34@gmail.com>
2018-04-03 23:37:00 +0900

    no message

--- public/lib/board/board.js
+++ public/lib/board/board.js
@@ -157,7 +157,7 @@ module.exports.board_con = function(app,mongoose){
         // Hitting GET request to the URL, Google will respond with success or error scenario.
         app.post(verificationUrl, function(error,response) {
           console.log("response ======");
-          console.log(response);
+          console.log(response.body);
           // Success will be true or false depending upon captcha validation.
           if(body.success !== undefined && !body.success) {
             return response.send("<script>location.href='"+request.session.urlpath+"';alert('스팸방지 코드를 다시 확인해주세요');</script>");
