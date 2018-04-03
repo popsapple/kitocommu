b23d415aa1a02cfce79f7b529deb7bafd4836cfa
hyeongaram <saysun34@gmail.com>
2018-04-04 00:28:41 +0900

    no message

--- public/lib/board/board.js
+++ public/lib/board/board.js
@@ -160,12 +160,12 @@ module.exports.board_con = function(app,mongoose,request_fun){
       // req.connection.remoteAddress will provide IP address of connected user.
       var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + request.body['g-recaptcha-response'] + "&remoteip=" + request.connection.remoteAddress;
       // Hitting GET request to the URL, Google will respond with success or error scenario.
-      request_fun(verificationUrl,function(error,response,body) {
+      request_fun(verificationUrl,function(error, response, body) {
         console.log("response ====== :: "+request_.body.board_table_id);
         body = JSON.parse(body);
         // Success will be true or false depending upon captcha validation.
         if(body.success !== undefined && !body.success) {
-          return response.send("<script>location.href='"+request.session.urlpath+"';alert('스팸방지 코드를 다시 확인해주세요');</script>");
+          return response_.send("<script>location.href='"+request_.session.urlpath+"';alert('스팸방지 코드를 다시 확인해주세요');</script>");
         }
         var board_id = 'Board_'+(request_.body.board_table_id);
         Board.write(request_.body,request_,response_,mongoose,board_id,'save');
