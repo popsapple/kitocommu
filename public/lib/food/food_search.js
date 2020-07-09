538f2fdfe9369aa0e5860da0b10e0bab8d6b7b65
hyeongaram <saysun34@innodep.com>
2020-07-09 12:38:18 +0900

    no message

--- public/lib/food/food_search.js
+++ public/lib/food/food_search.js
@@ -15,13 +15,9 @@ module.exports.food_search = function (app) {
         // result
         console.log("보내기에러" + JSON.stringify(result.Root.food[0]));
         send_res = result.Root.food.filter((item, idx) => {
-          if (idx == 0) console.log("이름보기" + item.name[0]);
           return item.name[0].indexOf(request.body.foodname) !== -1;
         });
-        console.log("정렬시" + JSON.stringify(send_res));
       });
-
-      console.log("보낼때" + JSON.stringify(send_res));
       response
         .send({
           foodname: request.body.foodname,
