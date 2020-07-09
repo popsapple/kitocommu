cfa3659926820896018cc9ded4bd4d65d743ad8c
hyeongaram <saysun34@innodep.com>
2020-07-09 12:33:58 +0900

    no message

--- public/lib/food/food_search.js
+++ public/lib/food/food_search.js
@@ -10,21 +10,22 @@ module.exports.food_search = function (app) {
   app.post("/food_search", function (request, response) {
     console.log("===============================================");
     (async () => {
-      var result = [];
+      var send_res = [];
       await parseString(foodxml.foodlist, function (err, result) {
         // result
         console.log("보내기에러" + JSON.stringify(result.Root.food[0]));
-        result = result.Root.food.filter((item, idx) => {
+        send_res = result.Root.food.filter((item, idx) => {
           if (idx == 0) console.log("이름보기" + item.name[0]);
           return item.name[0].indexOf(request.body.foodname) !== -1;
         });
+        console.log("정렬시" + JSON.stringify(send_res));
       });
 
-      console.log("보낼때" + JSON.stringify(result));
+      console.log("보낼때" + JSON.stringify(send_res));
       response
         .send({
           foodname: request.body.foodname,
-          foodlist: JSON.stringify(result),
+          foodlist: JSON.stringify(send_res),
         })
         .end();
     })();
